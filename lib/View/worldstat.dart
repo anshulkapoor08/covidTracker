1d5aa24cddcd5d16c3cf0d0895c740d8a32110a0
Anshul Kapoor <anshulkapoor306@gmail.com>
2024-07-25 22:19:15 +0530

    First UI done

--- lib/View/worldstat.dart
+++ lib/View/worldstat.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class WorldStateScreen extends StatefulWidget {
@@ -42,19 +43,87 @@ class _WorldStateScreenState extends State<WorldStateScreen>
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(15),
           child: Column(
             children: [
-              SizedBox(height: MediaQuery.of(context).size.height*0.01,),
-              const PieChart(dataMap: { "Total" : 20,
-              "Recovered": 15,
-              "Death": 5})
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              PieChart(
+                dataMap: const {"Total": 20, "Recovered": 15, "Death": 5},
+                chartRadius: MediaQuery.of(context).size.width * 3 / 2,
+                legendOptions:
+                    const LegendOptions(legendPosition: LegendPosition.left),
+                animationDuration: const Duration(milliseconds: 1200),
+                chartType: ChartType.ring,
+                colorList: colorList,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.06),
+                child: Card(
+                    child: Column(
+                  children: [
+                    ReusableRow(
+                      title: 'Total',
+                      value: '200',
+                    ),
+                    ReusableRow(
+                      title: 'Total',
+                      value: '200',
+                    ),
+                    ReusableRow(
+                      title: 'Total',
+                      value: '200',
+                    ),
+                    ReusableRow(
+                      title: 'Total',
+                      value: '200',
+                    ),
+                  ],
+                )),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff1aa260)),
+                child:const Center(
+                    child: Text(
+                  'Track Countries',
+                  style: TextStyle(color: Colors.black),
+                )),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+  ReusableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text(title), Text(value)],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Divider(),
+        ],
       ),
     );
   }
